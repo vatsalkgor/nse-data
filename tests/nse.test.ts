028dc952e24f18cadea543df9b9abd23e9146580
vatsalkgor <vatsalkgor@gmail.com>
2021-01-04 14:43:48 +1300

    stock-codes working with test

---
+++ tests/nse.test.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import NSE from '../src/index';
+const nse = new NSE();
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('getStockCodes', () => {
+  it('checks correct response', async () => {
+    const resp = {
+      data:
+        'SYMBOL,NAME OF COMPANY,TYPE\nAXISBANK,AXIS Bank Ltd., EQ\nVOLTAS,Voltas Limite, EQ\nWABAG,VA Tech Wabag Limited,EQ',
+    };
+    mockedAxios.get.mockResolvedValue(resp);
+    const codes = await nse.getStockCodes();
+    expect(codes.length).toBe(3);
+  });
+  it('expects rejection', () => {
+    mockedAxios.get.mockRejectedValueOnce('Invalid URL');
+    const codes = nse.getStockCodes();
+    expect(codes).rejects.toThrow('Invalid URL');
+  });
+});
